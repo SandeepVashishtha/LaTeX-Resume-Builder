647e2e75fcf7593dc90262d56865a4175710613e
Sandeep Vashishtha <sandeepvashishtha@outlook.in>
2024-08-01 00:22:22 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,24 +1,7 @@
+// backend/controllers/authController.js
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-
-exports.register = async (req, res) => {
-  const { username, email, password } = req.body;
-  try {
-    let user = await User.findOne({ email });
-    if (user) return res.status(400).json({ msg: 'User already exists' });
-    user = new User({ username, email, password });
-    await user.save();
-    const payload = { user: { id: user.id } };
-    jwt.sign(payload, 'secret', { expiresIn: 3600 }, (err, token) => {
-      if (err) throw err;
-      res.json({ token });
-    });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
-  }
-};
 
 exports.login = async (req, res) => {
   const { email, password } = req.body;
