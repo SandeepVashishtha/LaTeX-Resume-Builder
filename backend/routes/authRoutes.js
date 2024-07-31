660fb799ab234be19b9dbf31c774c00ee1199b3c
Sandeep Vashishtha <sandeepvashishtha@outlook.in>
2024-08-01 00:22:26 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,8 @@
+// backend/routes/authRoutes.js
 const express = require('express');
-const { register, login } = require('../controllers/authController');
+const { login } = require('../controllers/authController');
 const router = express.Router();
 
-router.post('/register', register);
 router.post('/login', login);
 
 module.exports = router;
