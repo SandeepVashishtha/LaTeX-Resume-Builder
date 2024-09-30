d893d59e6ad6e391d73a8950a58d8e8af0f183d1
Sandeep Vashishtha <sandeepvashishtha@outlook.in>
2024-09-30 13:59:50 +0530

    fixes mocha

---
+++ server/test/example.test.js
@@ -0,0 +1,8 @@
+// example.test.js
+const assert = require('assert');
+
+describe('Example Test', function() {
+    it('should return true', function() {
+        assert.strictEqual(true, true);
+    });
+});
